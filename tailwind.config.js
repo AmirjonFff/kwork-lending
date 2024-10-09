ca3fa6673c25ac06aae0fc7c9771e21720db34d1
Aminjon Ffff <fayzmuhammadamirjoni@gmail.com>
2024-10-09 15:37:08 +0500

    add max min

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      maxWidth: {
-        'container': '1160px',
+      fontFamily: {
+        bonnet: ['BonnetGrotesqueNr', 'sans-serif'],
       },
     },
   },
