35aac1c383295c2693cb05146c9e78cbfbe4263e
Aminjon Ffff <fayzmuhammadamirjoni@gmail.com>
2024-10-09 16:10:23 +0500

    finish 2

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
                         alt="Logo"
                         className="h-16"
                     />
-                    <nav className="flex items-center space-x-6 text-white ffnav">
+                    <nav className="flex items-center space-x-8 text-white ffnav">
                         <a href="#about" className="hidden md:block hover:text-yellow-400">About us</a>
                         <a href="#services" className="hidden md:block hover:text-yellow-400">Services</a>
                         <a href="#players" className="hidden md:block hover:text-yellow-400">Players portfolio</a>
@@ -25,13 +25,11 @@ const Home = () => {
 
             <div className="flex flex-col items-center justify-center h-full text-center mycontainer">
                 <div className="w-full max-w-container">
-                    <div>
-                        <h1 className="font-bold text-yellow-400 ffJOKS">JOKS SOCCER ACADEMY</h1>
-                    </div>
-                    <p className="text-white ffTRANS">TRANSFORMING LIVES THROUGH</p>
-                    <p className="ffFOOT text-border ">FOOTBALL AND EDUCATION!</p>
+                    <h1 className="font-bold text-yellow-400 ffJOKS">JOKS SOCCER ACADEMY</h1>
+                    <p className="my-2 tracking-tighter text-white ffTRANS">TRANSFORMING LIVES THROUGH</p>
+                    <p className="tracking-tighter ffFOOT text-border">FOOTBALL AND EDUCATION!</p>
 
-                    <button className="mt-6 text-black bg-yellow-400 rounded-full ffABOUT hover:bg-yellow-500">About us</button>
+                    <button className="font-bold text-black bg-yellow-400 rounded-full mt-9 ffABOUT hover:bg-yellow-500">About us</button>
                 </div>
             </div>
         </div>
